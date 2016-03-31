90a3e18d589883130e031472a4a14449283a3c27
dmibre <dmibre@greeny.cs.tlu.ee>
2016-03-31 13:16:03 +0300

    Added this functionality to the app

--- edit.php
+++ edit.php
@@ -13,6 +13,43 @@
 	}
 	
 	$mysql = new mysqli("localhost", $db_username, $db_password, "webpr2016_dmibre");
+	
+	
+	//may be user wants to update the data after clicking the button
+	
+	if(isset($_GET["to"]) && isset($_GET["message"])){ 
+	
+	echo "User modified data, tries to save";
+	
+	//should be validation?
+	
+	$stmt = $mysql->prepare("UPDATE messages_sample SET to=?, message=? WHERE id=?");
+	
+	echo $mysql->error;
+	
+	$stmt->bind_param("ssi", $_GET["to"], $_GET["message"], $_GET["edit"]);
+	if($stmt->execute()){
+				
+				echo "saved successfully"; 
+				
+				
+			//option number 1
+			header("Location: table.php");
+			//exit
+			
+			//option number 2
+			//$recipient = $_GET["to"];
+			//$message = $_GET["message"];
+			//$id = $_GET["edit"];
+				
+			}else{
+				echo $stmt->error;
+			}
+			
+			
+		}else{
+		
+	}
 		
 	$stmt = $mysql->prepare("SELECT id, recipient, message FROM messages_sample WHERE id=?");
 	
@@ -41,11 +78,14 @@
 <h2> First application </h2>
 
 <form method="get">
+
+   <input hidden name="edit" value="<?=$id;?>"><br><br>
+
 	<label for="to">to:* <label>
-	<input type="text" name="to"><br><br>
+	<input type="text" name="to" value="<?php echo $recipient; ?>"><br><br>
 	
 	<label for="message">Message:* <label>
-	<input type="text" name="message"><br><br>
+	<input type="text" name="message" value="<?php echo $message; ?>"><br><br>
 	
 	<!-- This is the save button-->
 	<input type="submit" value="Save to DB">
